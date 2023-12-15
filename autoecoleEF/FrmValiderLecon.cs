4a2fe894a7922ecdfeb83f3673997c7cd7d353f8
siomathisa <vidueira.mathis.sio@gmail.com>
2023-12-15 16:47:03 +0100

    test

--- autoecoleEF/FrmValiderLecon.cs
+++ autoecoleEF/FrmValiderLecon.cs
@@ -17,8 +17,44 @@ namespace autoecoleEF
         {
             InitializeComponent();
             this.mesDonneesEF = new autoEcoleEntities();
-            this.bdgSlecon.DataSource = mesDonneesEF.lecon.ToList();
+            this.mesDonneesEF = mesDonneesEF;
+            this.dataGridView1.AutoGenerateColumns = false;
+
+            //creation source donnees
+            var lesLecons = from l in mesDonneesEF.lecon
+                            where l.effectueeOui_Non == 0
+                            select l;
+
+            this.bdgSlecon.DataSource = lesLecons.ToList();
+
             this.bdgSvehicule.DataSource = mesDonneesEF.vehicule.ToList();
+
+            DataGridViewTextBoxColumn txtColonneDate = new DataGridViewTextBoxColumn();
+            txtColonneDate.HeaderText = "date";
+            txtColonneDate.Name = "date";
+            txtColonneDate.DataPropertyName = "date";
+            this.dataGridView1.Columns.Add(txtColonneDate);
+
+            DataGridViewTextBoxColumn txtColonneHeure = new DataGridViewTextBoxColumn();
+            txtColonneHeure.HeaderText = "heure";
+            txtColonneHeure.Name = "heure";
+            txtColonneHeure.DataPropertyName = "heure";
+            this.dataGridView1.Columns.Add(txtColonneHeure);
+
+            DataGridViewComboBoxColumn cmbColonneVehicule = new DataGridViewComboBoxColumn();
+            cmbColonneVehicule.HeaderText = "vehicule";
+            cmbColonneVehicule.Name = "vehicule";
+            cmbColonneVehicule.DisplayMember = "numImma";
+            cmbColonneVehicule.DataPropertyName = "numImmaVehicule";
+            cmbColonneVehicule.DataSource = this.bdgSvehicule;
+            this.dataGridView1.Columns.Add(cmbColonneVehicule);
+
+            DataGridViewCheckBoxColumn chkColonneOk = new DataGridViewCheckBoxColumn();
+            chkColonneOk.HeaderText = "effectuee";
+            chkColonneOk.Name = "effectuee";
+            chkColonneOk.DataPropertyName = "effectueeOui_Non";
+            this.dataGridView1.Columns.Add(chkColonneOk);
+            this.dataGridView1.DataSource = this.bdgSlecon;
         }
 
         private void FrmValiderLecon_Load(object sender, EventArgs e)
@@ -29,6 +65,7 @@ namespace autoecoleEF
 
         private void btnEnregistrer_Click(object sender, EventArgs e)
         {
+            this.bdgSlecon.EndEdit();
             this.mesDonneesEF.SaveChanges();
             MessageBox.Show("Enregistrement validé :)");
         }
